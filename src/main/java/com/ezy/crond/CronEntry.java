70bcbab82b81e04913ea95f7afdd5bebd6b08e41
Erik <allareri@gmail.com>
2019-11-29 10:26:14 -0700

    Initial complete parser

--- src/main/java/com/ezy/crond/CronEntry.java
+++ src/main/java/com/ezy/crond/CronEntry.java
@@ -1,5 +1,117 @@
 package com.ezy.crond;
 
-public class CronEntry {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class CronEntry { // CronTime since it's not an actual entry, no command to execute
+    private final Field minutes;
+    private final Field hours;
+    private final Field daysOfMonth;
+    private final Field months;
+    private final Field daysOfWeek;
+    /* daysOfMonth and daysOfWeek are treated as an OR clause */
+
+    enum FieldType {
+        NUMBER,    /* normal single digit */
+        WILDCARD,  /* '*' */
+        LIST,      /* list of comma-separated digits '1,2,3,4' */
+        RANGE,     /* range of numbers from low to high '2-6' */
+        INTERVAL,  /* interval specifying execution every nth time '*\/10' */
+        ERROR      /* invalid specification, numbers outside of bounds of field */
+    }
+
+    static class Field {
+        private FieldType type;
+        private List<Integer> values = new ArrayList<>(2);
+
+        public Field(FieldType type) {
+            this.type = type;
+        }
+        public FieldType getType() { return type; }
+        public List<Integer> getValues() { return values; }
+        public void addValue(int val) {
+            values.add(val);
+        }
+
+        @Override
+        public String toString() {
+            return "type: " + type.name() +
+                " values: " + values;
+        }
+    }
+
+    CronEntry(Field minutes,
+              Field hours,
+              Field daysOfMonth,
+              Field months,
+              Field daysOfWeek) {
+        this.minutes = minutes;
+        this.hours = hours;
+        this.daysOfMonth = daysOfMonth;
+        this.months = months;
+        this.daysOfWeek = daysOfWeek;
+    }
+
+
+    public static CronEntry parse(String crontab) {
+        String[] parts = crontab.split(" ");
+        if (parts.length < 5 || parts.length > 5) {
+            throw new IllegalArgumentException("Cron time entry specification must contain only 5 parts: " +
+                                               "'minute hour day_of_month month day_of_week'");
+        }
+
+        Field[] fields = new Field[5];
+        for (int i = 0; i < parts.length; i++) {
+            fields[i] = parseField(parts[i]);
+        }
+        return new CronEntry(fields[0], fields[1], fields[2], fields[3], fields[4]);
+    }
+
+    static Field parseField(String field) {
+        if (field.equalsIgnoreCase("*")) {
+            return new Field(FieldType.WILDCARD);
+        }
+        if (field.contains("*/")) {
+            String[] parts = field.split("\\*/"); /* Have to escape the '*' because it has meaning in regex  */
+            /* The split results in an empty string as the first element in the 'parts' array */
+            if (parts.length != 2) {
+                System.out.println("INTERVAL is invalid");
+                // IllegalArgument
+            }
+            Field f = new Field(FieldType.INTERVAL);
+            f.addValue(Integer.valueOf(parts[1]));
+            return f;
+        }
+        if (field.contains("-")) {
+            String[] parts = field.split("-");
+            if (parts.length != 2) {
+                System.out.println("RANGE is invalid");
+                // IllegalArgument
+            }
+            Field f = new Field(FieldType.RANGE);
+            f.addValue(Integer.valueOf(parts[0]));  /* Beginning of range */
+            f.addValue(Integer.valueOf(parts[1]));  /* End of range */
+            return f;
+        }
+        if (field.contains(",")) {
+            String[] parts = field.split(",");
+            Field f = new Field(FieldType.LIST);
+            for(int i = 0; i < parts.length; i++) {
+                f.addValue(Integer.valueOf(parts[i]));
+            }
+            return f;
+        }
+
+        Field f = new Field(FieldType.NUMBER);
+        f.addValue(Integer.valueOf(field));
+        return f;
+    }
+
+
+    public Field getMinutes() { return minutes; }
+    public Field getHours() { return hours; }
+    public Field getDaysOfMonth() { return daysOfMonth; }
+    public Field getMonths() { return months; }
+    public Field getDaysOfWeek() { return daysOfWeek; }
 }
--- src/test/java/com/ezy/crond/CronEntryTest.java
+++ src/test/java/com/ezy/crond/CronEntryTest.java
@@ -1,4 +1,106 @@
 package com.ezy.crond;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CronEntryTest {
+
+    @Test
+    public void testParseAllNumbers() {
+        String crontab = "1 2 3 4 5";
+        CronEntry entry = CronEntry.parse(crontab);
+
+        assertEquals(1, (int) entry.getMinutes().getValues().get(0));
+        assertEquals(CronEntry.FieldType.NUMBER, entry.getMinutes().getType());
+        assertEquals(2, (int) entry.getHours().getValues().get(0));
+        assertEquals(CronEntry.FieldType.NUMBER, entry.getHours().getType());
+        assertEquals(3, (int) entry.getDaysOfMonth().getValues().get(0));
+        assertEquals(CronEntry.FieldType.NUMBER, entry.getDaysOfMonth().getType());
+        assertEquals(4, (int) entry.getMonths().getValues().get(0));
+        assertEquals(CronEntry.FieldType.NUMBER, entry.getMonths().getType());
+        assertEquals(5, (int) entry.getDaysOfWeek().getValues().get(0));
+        assertEquals(CronEntry.FieldType.NUMBER, entry.getDaysOfWeek().getType());
+    }
+
+    @Test
+    public void testParseWithWildcard() {
+        String crontab = "1 2 3 4 *";
+
+        CronEntry entry = CronEntry.parse(crontab);
+
+        assertEquals(CronEntry.FieldType.NUMBER, entry.getMonths().getType());
+        assertEquals(CronEntry.FieldType.WILDCARD, entry.getDaysOfWeek().getType());
+    }
+
+    @Test
+    public void testParseRange() {
+        String crontab = "1-14 2 3 4 5";
+
+        CronEntry entry = CronEntry.parse(crontab);
+
+        assertEquals(CronEntry.FieldType.RANGE, entry.getMinutes().getType());
+        assertEquals(2, (int) entry.getMinutes().getValues().size());
+        assertEquals(1, (int) entry.getMinutes().getValues().get(0));  // Begin range
+        assertEquals(14, (int) entry.getMinutes().getValues().get(1)); // End range
+
+        assertEquals(CronEntry.FieldType.NUMBER, entry.getHours().getType());
+        assertEquals(CronEntry.FieldType.NUMBER, entry.getMonths().getType());
+    }
+
+    @Test
+    public void testParseList() {
+        String crontab = "1,3,5,8,13 * * * *";
+
+        CronEntry entry = CronEntry.parse(crontab);
+
+        assertEquals(CronEntry.FieldType.LIST, entry.getMinutes().getType());
+        assertEquals(5, (int) entry.getMinutes().getValues().size());
+        assertEquals(1, (int) entry.getMinutes().getValues().get(0));
+        assertEquals(3, (int) entry.getMinutes().getValues().get(1));
+        assertEquals(5, (int) entry.getMinutes().getValues().get(2));
+        assertEquals(8, (int) entry.getMinutes().getValues().get(3));
+        assertEquals(13, (int) entry.getMinutes().getValues().get(4));
+
+        assertEquals(CronEntry.FieldType.WILDCARD, entry.getHours().getType());
+        assertEquals(CronEntry.FieldType.WILDCARD, entry.getMonths().getType());
+    }
+
+    @Test
+    public void testParseInterval() {
+        String crontab = "*/5 * * * *";
+
+        CronEntry entry = CronEntry.parse(crontab);
+
+        assertEquals(CronEntry.FieldType.INTERVAL, entry.getMinutes().getType());
+        assertEquals(1, (int) entry.getMinutes().getValues().size());
+        assertEquals(5, (int) entry.getMinutes().getValues().get(0));
+    }
+
+    @Test
+    public void testParseMultipleTypes() {
+        String crontab = "*/2 * 10,11,12 3-6 11";
+
+        CronEntry entry = CronEntry.parse(crontab);
+
+        assertEquals(CronEntry.FieldType.INTERVAL, entry.getMinutes().getType());
+        assertEquals(2, (int) entry.getMinutes().getValues().get(0));
+
+        assertEquals(CronEntry.FieldType.WILDCARD, entry.getHours().getType());
+
+        assertEquals(CronEntry.FieldType.LIST, entry.getDaysOfMonth().getType());
+        assertEquals(3, (int) entry.getDaysOfMonth().getValues().size());
+        assertEquals(10, (int) entry.getDaysOfMonth().getValues().get(0));
+        assertEquals(11, (int) entry.getDaysOfMonth().getValues().get(1));
+        assertEquals(12, (int) entry.getDaysOfMonth().getValues().get(2));
+
+        assertEquals(CronEntry.FieldType.RANGE, entry.getMonths().getType());
+        assertEquals(2, (int) entry.getMonths().getValues().size());
+        assertEquals(3, (int) entry.getMonths().getValues().get(0));
+        assertEquals(6, (int) entry.getMonths().getValues().get(1));
+
+        assertEquals(CronEntry.FieldType.NUMBER, entry.getDaysOfWeek().getType());
+        assertEquals(11, (int) entry.getDaysOfWeek().getValues().get(0));
+    }
 }
