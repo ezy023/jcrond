ac5eeadfabcf6677d02e07c7c913e57f64698899
Erik <allareri@gmail.com>
2019-12-01 09:38:05 -0700

    Basic next execution working with the exception of days overflow calculations

--- src/main/java/com/ezy/crond/CronTime.java
+++ src/main/java/com/ezy/crond/CronTime.java
@@ -1,7 +1,10 @@
 package com.ezy.crond;
 
+import java.time.ZonedDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 public class CronTime { // CronTime since it's not an actual entry, no command to execute
@@ -22,16 +25,89 @@ public class CronTime { // CronTime since it's not an actual entry, no command t
     }
 
     static class Field {
-        private FieldType type;
-        private List<Integer> values = new ArrayList<>(2);
+        boolean overflow;
+        int max, min;
+        final FieldType type;
+        List<Integer> values = new ArrayList<>(2);
 
         public Field(FieldType type) {
             this.type = type;
         }
+
+        public Field(FieldType type, int max, int min) {
+            this.type = type;
+            this.max = max;
+            this.min = min;
+        }
+
         public FieldType getType() { return type; }
         public List<Integer> getValues() { return values; }
         public void addValue(int val) {
             values.add(val);
+        }
+
+        public int getMax() { return max; }
+        public int getMin() { return min; }
+
+        public void setOverflow(boolean overflow) { this.overflow = overflow; }
+        public boolean getOverflow() { return overflow; }
+
+        // TODO Unit test this method separately from nextExecution
+        public int getNext(int current, int min, int max) {
+            /* Here we are already being given an int current that has been adjusted for overflow */
+            /* also need to do max checking for current */
+            switch(type) {
+            case WILDCARD:
+                if (current > max) {
+                    setOverflow(true);
+                    return min;
+                }
+                return current;
+            case NUMBER:
+                if (current > values.get(0)) {
+                    setOverflow(true);
+                }
+                return values.get(0);
+            case RANGE:
+                if (current >= values.get(0) && current <= values.get(1)) {
+                    return current;
+                }
+                setOverflow(true);
+                return values.get(0);
+            case LIST:
+                if (values.contains(current)) {
+                    return current;
+                }
+                for(Integer val : values) {
+                    if (current < val) {
+                        return val;
+                    }
+                }
+                /* No values qualified which means the current is beyond the range of the list, have to overflow */
+                setOverflow(true);
+                return values.get(0);
+            case INTERVAL:
+                if (current % values.get(0) == 0) {
+                    return current;
+                }
+                while(current++ <= max) {
+                    if (current % values.get(0) == 0) {
+                        return current;
+                    }
+                }
+                setOverflow(true);
+                current = min;
+                while(current <= max) {
+                    if (current % values.get(0) == 0) {
+                        return current;
+                    }
+                    current++;
+                }
+            default:
+                break;
+            }
+            // TODO Something wrong here?
+            return values.get(0);
         }
 
         @Override
@@ -41,11 +117,19 @@ public class CronTime { // CronTime since it's not an actual entry, no command t
         }
     }
 
+    static class MinuteField extends Field {
+        MinuteField(FieldType type, int max, int min) { super(type, max, min); }
+
+        static MinuteField fromField(Field field) {
+            return new MinuteField(field.getType(), 0, 59);
+        }
+    }
+
     CronTime(Field minutes,
-              Field hours,
-              Field daysOfMonth,
-              Field months,
-              Field daysOfWeek) {
+             Field hours,
+             Field daysOfMonth,
+             Field months,
+             Field daysOfWeek) {
         this.minutes = minutes;
         this.hours = hours;
         this.daysOfMonth = daysOfMonth;
@@ -108,10 +192,92 @@ public class CronTime { // CronTime since it's not an actual entry, no command t
         return f;
     }
 
-
     public Field getMinutes() { return minutes; }
     public Field getHours() { return hours; }
     public Field getDaysOfMonth() { return daysOfMonth; }
     public Field getMonths() { return months; }
     public Field getDaysOfWeek() { return daysOfWeek; }
+
+    ZonedDateTime nextMinute(ZonedDateTime current) {
+        /* Always add one to the minute to avoid a job executing multiple times in one minute */
+        int next = getMinutes().getNext(current.getMinute() + 1, 0, 59);
+        return current.withMinute(next);
+    }
+
+    ZonedDateTime nextHour(ZonedDateTime current) {
+        int nextBase = current.getHour();
+        if (getMinutes().getOverflow()) {
+            nextBase += 1;
+        }
+        int next = getHours().getNext(nextBase, 0, 23);
+        return current.withHour(next);
+    }
+
+    /* TODO add check and logic if one of the day specifications is a wildcard */
+    /* This will return the next day in terms of day of the month */
+    ZonedDateTime nextDay(ZonedDateTime current) {
+        int nextBaseDOW = current.getDayOfWeek().getValue();
+        int nextBaseDOM = current.getDayOfMonth();
+        if (getHours().getOverflow()) {
+            nextBaseDOW += 1;
+            nextBaseDOM += 1;
+        }
+        int nextDOW = getDaysOfWeek().getNext(nextBaseDOW, 1, 7);
+        int dowDiff = dayOfWeekDiff(nextBaseDOW, nextDOW);
+        int nextDOWinDOM = nextBaseDOM + dowDiff;
+
+        int nextDOM = getDaysOfMonth().getNext(nextBaseDOM, 1, current.getMonth().length(isLeapYear(current.getYear())));
+        int next = Math.min(nextDOWinDOM, nextDOM);
+        System.out.println(String.format("Next DOM: %d. Next DOW: %d", nextDOM, nextDOW));
+        System.out.println(String.format("nextDOWinDOM: %d. nextDOM: %d", nextDOWinDOM, nextDOM));
+        return current.withDayOfMonth(next);
+    }
+
+    int dayOfWeekDiff(int current, int next) {
+        if (current < next) {
+            return next - current;
+        } else {
+            int toAdd = next == 7 ? 0 : next;
+            return 7 - current + toAdd;
+        }
+    }
+
+
+    ZonedDateTime nextMonth(ZonedDateTime current) {
+        int nextBase = current.getMonthValue();
+        if (getDaysOfMonth().getOverflow()) {
+            nextBase += 1;
+        }
+        int next = getMonths().getNext(nextBase, 1, 12);
+
+        return current.withMonth(next);
+    }
+
+    ZonedDateTime nextYear(ZonedDateTime current) {
+        int next = getMonths().getOverflow() ? current.getYear() + 1 : current.getYear();
+        return current.withYear(next);
+    }
+
+    boolean isLeapYear(int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        return cal.getActualMaximum(Calendar.DAY_OF_YEAR) > 365;
+    }
+
+    /* need get next unit methods
+       in the get next unit, take a zdt, check if there is overflow from the unit beloww
+       if there is, add on to the unit from the zdt and pass it to the get next int method
+       if there is no overflow the just pass the unit to the get next int.
+       For minutes always add one to the current value
+    */
+
+    public ZonedDateTime nextExecution(ZonedDateTime currentTime) {
+        // Reset all overflow switches
+        ZonedDateTime next = nextMinute(currentTime);
+        next = nextHour(next);
+        next = nextDay(next);
+        next = nextMonth(next);
+        next = nextYear(next);
+        return next;
+    }
 }
--- src/test/java/com/ezy/crond/CronTimeTest.java
+++ src/test/java/com/ezy/crond/CronTimeTest.java
@@ -1,5 +1,11 @@
 package com.ezy.crond;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+import java.time.ZoneOffset;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -103,4 +109,166 @@ public class CronTimeTest {
         assertEquals(CronTime.FieldType.NUMBER, cronTime.getDaysOfWeek().getType());
         assertEquals(11, (int) cronTime.getDaysOfWeek().getValues().get(0));
     }
+
+    @Test
+    public void testMinuteFieldFromField() {
+        CronTime.Field field = new CronTime.Field(CronTime.FieldType.NUMBER);
+        CronTime.MinuteField minuteField = CronTime.MinuteField.fromField(field);
+
+        assertNotNull(minuteField);
+        assertEquals(0, minuteField.getMax());
+        assertEquals(59, minuteField.getMin());
+    }
+
+    @Test
+    public void testNextExecutionHourly() {
+        String crontab = "5 * * * *";
+        CronTime cronTime = CronTime.parse(crontab);
+        ZonedDateTime fixedTime = ZonedDateTime.of(LocalDate.of(2019, 12, 1),
+                                                   LocalTime.MIDNIGHT,
+                                                   ZoneOffset.UTC);
+        ZonedDateTime expectedNextExecution = ZonedDateTime.of(LocalDate.of(2019, 12, 1),
+                                                               LocalTime.of(0, 5),
+                                                               ZoneOffset.UTC);
+        ZonedDateTime nextExecution = cronTime.nextExecution(fixedTime);
+
+        assertEquals(expectedNextExecution, nextExecution);
+        assertEquals(0, nextExecution.getHour());
+        assertEquals(5, nextExecution.getMinute());
+    }
+
+    @Test
+    public void testNextExecutionHourlyNextDay() {
+        String crontab = "5 * * * *";
+        CronTime cronTime = CronTime.parse(crontab);
+        ZonedDateTime fixedTime = ZonedDateTime.of(LocalDate.of(2019, 12, 1),
+                                                   LocalTime.of(23, 6),
+                                                   ZoneOffset.UTC);
+        ZonedDateTime expectedNextExecution = ZonedDateTime.of(LocalDate.of(2019, 12, 2),
+                                                               LocalTime.of(0, 5),
+                                                               ZoneOffset.UTC);
+        ZonedDateTime nextExecution = cronTime.nextExecution(fixedTime);
+
+        assertEquals(expectedNextExecution, nextExecution);
+        assertEquals(2, nextExecution.getDayOfMonth());
+        assertEquals(0, nextExecution.getHour());
+        assertEquals(5, nextExecution.getMinute());
+    }
+
+    @Test
+    public void testNextExecutionNextMonthNextYear() {
+        String crontab = "5 * * * *";
+        CronTime cronTime = CronTime.parse(crontab);
+        ZonedDateTime fixedTime = ZonedDateTime.of(LocalDate.of(2019, 12, 31),
+                                                   LocalTime.of(23, 6),
+                                                   ZoneOffset.UTC);
+        ZonedDateTime expectedNextExecution = ZonedDateTime.of(LocalDate.of(2020, 1, 1),
+                                                               LocalTime.of(0, 5),
+                                                               ZoneOffset.UTC);
+        ZonedDateTime nextExecution = cronTime.nextExecution(fixedTime);
+
+        assertEquals(expectedNextExecution, nextExecution);
+        assertEquals(2020, nextExecution.getYear());
+        assertEquals(1, nextExecution.getMonthValue());
+        assertEquals(1, nextExecution.getDayOfMonth());
+        assertEquals(0, nextExecution.getHour());
+        assertEquals(5, nextExecution.getMinute());
+    }
+
+    @Test
+    public void testNextExcutionWithRangeValue() {
+        String crontab = "5 2-6 * * *";
+        CronTime cronTime = CronTime.parse(crontab);
+        ZonedDateTime fixedTime = ZonedDateTime.of(LocalDate.of(2019, 12, 1),
+                                                   LocalTime.of(3, 6),
+                                                   ZoneOffset.UTC);
+        ZonedDateTime expectedNextExecution = ZonedDateTime.of(LocalDate.of(2019, 12, 1),
+                                                               LocalTime.of(4, 5),
+                                                               ZoneOffset.UTC);
+        ZonedDateTime nextExecution = cronTime.nextExecution(fixedTime);
+
+        assertEquals(expectedNextExecution, nextExecution);
+    }
+
+    @Test
+    public void testNextExcutionWithListValue() {
+        String crontab = "5 * 1,3,5,8 * *";
+        CronTime cronTime = CronTime.parse(crontab);
+        ZonedDateTime fixedTime = ZonedDateTime.of(LocalDate.of(2019, 12, 1),
+                                                   LocalTime.of(23, 6),
+                                                   ZoneOffset.UTC);
+        ZonedDateTime expectedNextExecution = ZonedDateTime.of(LocalDate.of(2019, 12, 3),
+                                                               LocalTime.of(0, 5),
+                                                               ZoneOffset.UTC);
+        ZonedDateTime nextExecution = cronTime.nextExecution(fixedTime);
+
+        assertEquals(expectedNextExecution, nextExecution);
+    }
+
+    @Test
+    public void testNextExcutionWithIntervalValue() {
+        String crontab = "5 */5 * * *";
+        CronTime cronTime = CronTime.parse(crontab);
+        ZonedDateTime fixedTime = ZonedDateTime.of(LocalDate.of(2019, 12, 1),
+                                                   LocalTime.of(5, 6),
+                                                   ZoneOffset.UTC);
+        ZonedDateTime expectedNextExecution = ZonedDateTime.of(LocalDate.of(2019, 12, 1),
+                                                               LocalTime.of(10, 5),
+                                                               ZoneOffset.UTC);
+        ZonedDateTime nextExecution = cronTime.nextExecution(fixedTime);
+
+        assertEquals(expectedNextExecution, nextExecution);
+    }
+
+    @Test
+    public void testNextExcutionWithIntervalValueWithOverflow() {
+        String crontab = "5 */5 * * *"; // Execute at 5 after the hour every 5th hour
+        CronTime cronTime = CronTime.parse(crontab);
+        ZonedDateTime fixedTime = ZonedDateTime.of(LocalDate.of(2019, 12, 1),
+                                                   LocalTime.of(23, 5),
+                                                   ZoneOffset.UTC);
+        ZonedDateTime expectedNextExecution = ZonedDateTime.of(LocalDate.of(2019, 12, 2),
+                                                               LocalTime.of(0, 5),
+                                                               ZoneOffset.UTC);
+        ZonedDateTime nextExecution = cronTime.nextExecution(fixedTime);
+
+        assertEquals(expectedNextExecution, nextExecution);
+    }
+
+    @Test
+    public void testNextExcutionWithIntervalValueWithOverflowWithNonZeroMinValue() {
+        String crontab = "5 0 * */2 *"; // Every day at 0:05 of even months
+        CronTime cronTime = CronTime.parse(crontab);
+        ZonedDateTime fixedTime = ZonedDateTime.of(LocalDate.of(2019, 12, 31),
+                                                   LocalTime.of(23, 5),
+                                                   ZoneOffset.UTC);
+        ZonedDateTime expectedNextExecution = ZonedDateTime.of(LocalDate.of(2020, 2, 1),
+                                                               LocalTime.of(0, 5),
+                                                               ZoneOffset.UTC);
+        ZonedDateTime nextExecution = cronTime.nextExecution(fixedTime);
+
+        assertEquals(expectedNextExecution, nextExecution);
+    }
+    // TODO test next day with intersection of day of month and day of week
+    // need to test when both overflow, when either overflow and when neither overflow
+
+
+    @Test
+    public void testNextExecutionDayOfWeekBeforeNextDayOfMonth() {
+        String crontab = "5 0 6 * 3"; // Execute on the 6th of every month and every Wednesday
+        CronTime cronTime = CronTime.parse(crontab);
+        ZonedDateTime fixedTime = ZonedDateTime.of(LocalDate.of(2019, 12, 1), // Sunday
+                                                   LocalTime.of(0, 5),
+                                                   ZoneOffset.UTC);
+        ZonedDateTime expectedNextExecution = ZonedDateTime.of(LocalDate.of(2019, 12, 4),
+                                                               LocalTime.of(0, 5),
+                                                               ZoneOffset.UTC);
+        ZonedDateTime nextExecution = cronTime.nextExecution(fixedTime);
+
+        assertEquals(expectedNextExecution, nextExecution);
+    }
+
+
+
+
 }
